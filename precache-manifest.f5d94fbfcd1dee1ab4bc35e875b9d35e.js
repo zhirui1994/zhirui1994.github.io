d2c2e454e92ff5bd0a4b4c852f77676efe4aaeba
zhirui1994 <www.zhirui@foxmail.com>
2019-01-13 00:56:48 +0800

    use hashRputer

---
+++ precache-manifest.f5d94fbfcd1dee1ab4bc35e875b9d35e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "374d4540177fc982c86e",
+    "url": "/static/css/main.e3f2e349.chunk.css"
+  },
+  {
+    "revision": "374d4540177fc982c86e",
+    "url": "/static/js/main.374d4540.chunk.js"
+  },
+  {
+    "revision": "2d6b3c76a5d8e35a3201",
+    "url": "/static/js/1.2d6b3c76.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e76ad8a4ffd61dd6251d12d5ad3c3bb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.508ff4efdb104ef98ea61afc52dcf391.js"
+  "/precache-manifest.f5d94fbfcd1dee1ab4bc35e875b9d35e.js"
 );
 
 workbox.clientsClaim();
